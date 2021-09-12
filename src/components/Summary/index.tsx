506472117dec4d24ac49211cd99d2b4f4d8464d2
Luiz Gustavo Retka Darossi <luizgustavodarossi@gmail.com>
2021-09-12 16:33:17 -0300

    Finished

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,75 @@
+import { useTransactions } from '../../hooks/useTransactions';
+
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import totalImg from '../../assets/total.svg';
+
+import { Container } from "./styles";
+
+export function Summary() {
+
+    const { transactions } = useTransactions();
+
+    const summary = transactions.reduce((acc, transaction) => {
+        if (transaction.type === 'deposit') {
+            acc.deposit += transaction.amount;
+            acc.total += transaction.amount;
+        } else {
+            acc.withdraws += transaction.amount;
+            acc.total -= transaction.amount;
+        }
+
+        return acc;
+    }, {
+        'deposit': 0,
+        'withdraws': 0,
+        'total': 0,
+    });
+
+    return (
+        <Container>
+            <div>
+                <header>
+                    <p>Entradas</p>
+                    <img src={incomeImg} alt="entradas" />
+                </header>
+                <strong>
+                    {
+                        new Intl.NumberFormat('pt-BR', {
+                            style: 'currency',
+                            currency: 'BRL',
+                        }).format(summary.deposit)
+                    }
+                </strong>
+            </div>
+            <div>
+                <header>
+                    <p>Saídas</p>
+                    <img src={outcomeImg} alt="saídas" />
+                </header>
+                <strong>-
+                    {
+                        new Intl.NumberFormat('pt-BR', {
+                            style: 'currency',
+                            currency: 'BRL',
+                        }).format(summary.withdraws)
+                    }
+                </strong>
+            </div>
+            <div className="highlight-background">
+                <header>
+                    <p>Total</p>
+                    <img src={totalImg} alt="total" />
+                </header>
+                <strong>
+                    {
+                        new Intl.NumberFormat('pt-BR', {
+                            style: 'currency',
+                            currency: 'BRL',
+                        }).format(summary.total)
+                    }
+                </strong>
+            </div>
+        </Container>
+    );
+}
